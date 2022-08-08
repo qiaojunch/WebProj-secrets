ae43aa88f47eb4c93c4a874d368d8313aa9f42f6
qiaojunch <qiaojunch@gmail.com>
2022-08-07 17:32:49 -0700

    Level 6: google authenticates login

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-//require("dotenv").config();   // configure your secret info
+require("dotenv").config();   // configure your secret info
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -7,7 +7,10 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
 const passportLocalMongoose = require("passport-local-mongoose");
-
+// authenticates users using a Google account
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// declare mongoose-findorcreate package AND plugin to your Schema
+const findOrCreate = require("mongoose-findOrCreate")
 
 const app = express();
 
@@ -33,22 +36,59 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 // to use mongoose: create a model
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
-// use PLM
+
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-// use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
+// used to serialize the user for the session
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+   // where is this user.id going? Are we supposed to access this anywhere?
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+// use passport google authenticate to login
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SERECT,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        console.log(profile);
+        return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res){
     res.render("home");
 });
+
+// authenticate request from login or register page
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
+
 
 app.get("/register", function(req, res){
     res.render("register");
@@ -75,6 +115,7 @@ app.get("/secrets", function(req, res){
         res.redirect("/login");
     }
 });
+
 
 app.post("/register", function(req, res){
     // register user from passportLocalMongoose
@@ -109,6 +150,8 @@ app.post("/login", function(req, res){
         }
     })
 });
+
+
 
 
 
